2dc974ab1ffb90abf113923e550097db307fb02c
ZhanLiangUF <zliang@ufl.edu>
2017-02-23 14:21:09 -0500

    Initial Commit

---
+++ TraumaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TraumaApp
+//
+//  Created by Liang,Zhan W on 2/23/17.
+//  Copyright © 2017 UF Mobile Health Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
